5d50151f9c45e365753cebed029de0c89efbb84a
Ahmad Siddiqui <reachahmadsiddiqui@gmail.com>
2024-10-17 23:23:53 +0500

    Desktop nav & little bit of mobile nav

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,11 +1,12 @@
 import Link from "next/link";
 import Nav from "./Nav";
 import { Button } from "./ui/button";
+import MobileNav from "./MobileNav";
 
 const Header = () => {
   return (
     <header className="py-8 xl:py-12 text-white">
-      <div className="container mx-auto">
+      <div className="container mx-auto flex justify-between items-center">
         <Link href="/">
           <h1 className="text-4xl font-semibold">
             Ahmad
@@ -14,7 +15,7 @@ const Header = () => {
         </Link>
 
         {/* Desktop Nav */}
-        <div className="hidden xl:flex">
+        <div className="hidden xl:flex items-center gap-8">
           <Nav />
           <Link href="/">
             <Button>Hire Me</Button>
@@ -22,7 +23,9 @@ const Header = () => {
         </div>
 
         {/* Mobile Nav */}
-        <div className="xl:hidden">Mobile Nav</div>
+        <div className="xl:hidden">
+          <MobileNav />
+        </div>
       </div>
     </header>
   );
