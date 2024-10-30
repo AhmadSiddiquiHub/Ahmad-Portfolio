b752c519686a015ecddb674f1dd0bfde4f2730c4
Ahmad Siddiqui <reachahmadsiddiqui@gmail.com>
2024-10-30 23:45:21 +0500

    Contact form Functionality Implementation

--- app/contact/page.jsx
+++ app/contact/page.jsx
@@ -13,8 +13,11 @@ import {
   SelectValue,
 } from "@/components/ui/select";
 import { Textarea } from "@/components/ui/textarea";
-import { Item } from "@radix-ui/react-select";
 import { Button } from "@/components/ui/button";
+import { useRef, useState } from "react";
+import emailjs from "emailjs-com";
+import { toast, ToastContainer, Bounce } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 const ContactPage = () => {
   const info = [
@@ -34,78 +37,210 @@ const ContactPage = () => {
       description: "Ghalib Market Gulberg III, Lahore, Pakistan",
     },
   ];
+
+  const formRef = useRef();
+  const [form, setForm] = useState({
+    firstName: "",
+    lastName: "",
+    email: "",
+    number: "",
+    service: "",
+    message: "",
+  });
+  const [loading, setLoading] = useState(false);
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setForm({ ...form, [name]: value });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setLoading(true);
+
+    // Email Js Logic
+    emailjs
+      .send(
+        "service_3me8b08",
+        "template_do0xrj8",
+        {
+          from_name: form.firstName + " " + form.lastName,
+          to_name: "Ahmad Siddiqui",
+          from_email: form.email,
+          to_email: "reachahmadsiddiqui@gmail",
+          name: form.firstName + " " + form.lastName,
+          email: form.email,
+          number: form.number,
+          service: form.service,
+          message: form.message,
+        },
+        "_IEHuBroZ-eT4CBE7"
+      )
+      .then(
+        () => {
+          setLoading(false);
+          toast.success("Thank you for getting in touch!", {
+            position: "top-center",
+            autoClose: 5000,
+            hideProgressBar: false,
+            closeOnClick: true,
+            pauseOnHover: true,
+            draggable: true,
+            progress: undefined,
+            theme: "dark",
+            transition: Bounce,
+          });
+          setForm({
+            firstName: "",
+            lastName: "",
+            email: "",
+            number: "",
+            service: "",
+            message: "",
+          });
+        },
+        (error) => {
+          setLoading(false);
+          console.log(error);
+          toast.error("Something went wrong, please try again", {
+            position: "top-center",
+            autoClose: 5000,
+            hideProgressBar: false,
+            closeOnClick: true,
+            pauseOnHover: true,
+            draggable: true,
+            progress: undefined,
+            theme: "dark",
+            transition: Bounce,
+          });
+        }
+      );
+  };
+
   return (
-    <motion.section
-      initial={{ opacity: 0 }}
-      animate={{
-        opacity: 1,
-        transition: { delay: 2.4, duration: 0.4, ease: "easeIn" },
-      }}
-      className="py-6"
-    >
-      <div className="container mx-auto">
-        <div className="flex flex-col xl:flex-row gap-[30px]">
-          {/* form */}
-          <div className="xl:w-[54%] order-2 xl:order-none">
-            <form className="flex flex-col gap-6 p-10 bg-[#27272c] rounded-xl">
-              <h3 className="text-4xl text-accent">Let's work together</h3>
-              <p className="text-white/60">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                Dignissimos velit nemo, libero perspiciatis illum ut cupiditate.
-              </p>
-              {/* input */}
-              <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
-                <Input type="firstname" placeholder="First Name" />
-                <Input type="lastname" placeholder="Last Name" />
-                <Input type="email" placeholder="Email" />
-                <Input type="phone" placeholder="Phone Number" />
-              </div>
+    <>
+      <ToastContainer />
 
-              {/* select */}
-              <Select>
-                <SelectTrigger className="w-full">
-                  <SelectValue placeholder="Select a Service" />
-                </SelectTrigger>
-                <SelectContent>
-                  <SelectGroup>
-                    <SelectLabel>Select a service</SelectLabel>
-                    <SelectItem value="est">Web Development</SelectItem>
-                    <SelectItem value="cst">UI/UX Design</SelectItem>
-                    <SelectItem value="mst">Logo Design</SelectItem>
-                  </SelectGroup>
-                </SelectContent>
-              </Select>
+      <motion.section
+        initial={{ opacity: 0 }}
+        animate={{
+          opacity: 1,
+          transition: { delay: 2.4, duration: 0.4, ease: "easeIn" },
+        }}
+        className="py-6"
+      >
+        <div className="container mx-auto">
+          <div className="flex flex-col xl:flex-row gap-[30px]">
+            {/* form */}
+            <div className="xl:w-[54%] order-2 xl:order-none">
+              <form
+                ref={formRef}
+                onSubmit={handleSubmit}
+                className="flex flex-col gap-6 p-10 bg-[#27272c] rounded-xl"
+              >
+                <h3 className="text-4xl text-accent">Let's work together</h3>
+                <p className="text-white/60">
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                  Dignissimos velit nemo, libero perspiciatis illum ut
+                  cupiditate.
+                </p>
+                {/* input */}
+                <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
+                  <Input
+                    type="firstname"
+                    placeholder="First Name"
+                    name="firstName"
+                    value={form.firstName}
+                    onChange={handleChange}
+                    required
+                  />
+                  <Input
+                    type="lastname"
+                    placeholder="Last Name"
+                    name="lastName"
+                    value={form.lastName}
+                    onChange={handleChange}
+                    required
+                  />
+                  <Input
+                    type="email"
+                    placeholder="Email"
+                    name="email"
+                    value={form.email}
+                    onChange={handleChange}
+                    required
+                  />
+                  <Input
+                    type="phone"
+                    placeholder="Phone Number"
+                    name="number"
+                    value={form.number}
+                    onChange={handleChange}
+                    required
+                  />
+                </div>
 
-              <Textarea
-                className="h-[200px]"
-                placeholder="Type your message here"
-              />
-              <Button size="md" className="max-w-40">
-                Send Message
-              </Button>
-            </form>
-          </div>
-          {/* info */}
-          <div className="flex-1 flex items-center xl:justify-end order-1 xl:order-none mb-8 xl:mb-0">
-            <ul className="flex flex-col gap-10">
-              {info.map((item, index) => {
-                return (
-                  <li key={index} className="flex items-center gap-6">
-                    <div className="w-[52px] h-[52px] xl:w-[72px] xl:h-[72px] bg-[#27272c] text-accent rounded-md flex items-center justify-center">
-                      <div className="text-[28px]">{item.icon}</div>
-                    </div>
-                    <div className="flex-1">
-                      <p className="text-white/60">{item.title}</p>
-                      <h3 className="text-xl">{item.description}</h3>
-                    </div>
-                  </li>
-                );
-              })}
-            </ul>
+                {/* select */}
+                <Select
+                  onValueChange={(value) =>
+                    setForm({ ...form, service: value })
+                  }
+                >
+                  <SelectTrigger className="w-full">
+                    <SelectValue placeholder="Select a Service" />
+                  </SelectTrigger>
+                  <SelectContent>
+                    <SelectGroup>
+                      <SelectLabel>Select a service</SelectLabel>
+                      <SelectItem value="Web Development">
+                        Web Development
+                      </SelectItem>
+                      <SelectItem value="UI/UX Design">UI/UX Design</SelectItem>
+                      <SelectItem value="Logo Design">Logo Design</SelectItem>
+                    </SelectGroup>
+                  </SelectContent>
+                </Select>
+
+                <Textarea
+                  className="h-[200px]"
+                  placeholder="Type your message here"
+                  name="message"
+                  value={form.message}
+                  onChange={handleChange}
+                  required
+                />
+                <Button
+                  type="submit"
+                  size="md"
+                  className="max-w-40"
+                  disabled={loading}
+                >
+                  {loading ? "Sending..." : "Send Message"}
+                </Button>
+              </form>
+            </div>
+            {/* info */}
+            <div className="flex-1 flex items-center xl:justify-end order-1 xl:order-none mb-8 xl:mb-0">
+              <ul className="flex flex-col gap-10">
+                {info.map((item, index) => {
+                  return (
+                    <li key={index} className="flex items-center gap-6">
+                      <div className="w-[52px] h-[52px] xl:w-[72px] xl:h-[72px] bg-[#27272c] text-accent rounded-md flex items-center justify-center">
+                        <div className="text-[28px]">{item.icon}</div>
+                      </div>
+                      <div className="flex-1">
+                        <p className="text-white/60">{item.title}</p>
+                        <h3 className="text-xl">{item.description}</h3>
+                      </div>
+                    </li>
+                  );
+                })}
+              </ul>
+            </div>
           </div>
         </div>
-      </div>
-    </motion.section>
+      </motion.section>
+    </>
   );
 };
 
