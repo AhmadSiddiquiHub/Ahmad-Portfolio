2e3dfbc487b933fe9c9a97b4bc140c42218ac56c
Ahmad Siddiqui <reachahmadsiddiqui@gmail.com>
2024-10-30 21:56:24 +0500

    Work & Contact Page Implementation

--- components/Photo.jsx
+++ components/Photo.jsx
@@ -6,7 +6,13 @@ import Image from "next/image";
 const Photo = () => {
   return (
     <div className="w-full h-full relative">
-      <motion.div>
+      <motion.section
+      // initial={{ opacity: 0 }}
+      // animate={{
+      //   opacity: 1,
+      //   transition: { delay: 2, duration: 0.4, ease: "easeIn" },
+      // }}
+      >
         {/* Image */}
         <motion.div
           initial={{ opacity: 0 }}
@@ -14,7 +20,7 @@ const Photo = () => {
             opacity: 1,
             transition: { delay: 2.4, duration: 0.4, ease: "easeInOut" },
           }}
-          className="w-[298px] h-[298px] xl:w-[420px] xl:h-[370px] mix-blend-lighten absolute"
+          className="w-[298px] h-[298px] xl:w-[420px] xl:h-[370px] mix-blend-lighten absolute z-10"
         >
           <Image
             src="/assets/avatar.png"
@@ -53,7 +59,7 @@ const Photo = () => {
             }}
           />
         </motion.svg>
-      </motion.div>
+      </motion.section>
     </div>
   );
 };
